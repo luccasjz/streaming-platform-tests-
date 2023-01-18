07f2303008501a9148ba523d2cfb8b447e8d3be0
lukaskn2 <54250114+luccasjz@users.noreply.github.com>
2023-01-17 23:36:52 -0300

    stream cast dont function but have the estructues

---
+++ index.js
@@ -0,0 +1,85 @@
+
+import React, { Component } from 'react'
+import ./CSS.CSS
+class index
+ extends Component
+render(
+
+    return
+    <></>
+
+
+()
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class name ="container">
+      <div class name ="navbar">
+        <div class name ="menu">
+          <h3 class name ="logo">WETH<span>TV </span></h3>
+          <div class name ="hamburger-menu">
+            <div class name ="bar"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class name ="main-container">
+        <div class name ="main">
+          <header>
+            <div class name ="overlay">
+              <div class name ="inner">
+                <h2 class name ="title"><Fieldset>FUTURE is HERE </Fieldset></h2>
+                <p>
+                    INNOVATION TECHNOLOGY AND ACCESSIBILITY IN THE SEARCH FOR A BETTER FUTURE
+                </p>
+                <input  type="image" src="imagnes/sign-in-with-twitter-gray.png.twimg.1920.png "></src><A onclick="Evento()"><a href="Home.html"></button>
+
+                     <script>
+                   function Evento(){
+                        alert('evento funcionando')
+    }
+                                </script>
+              </div>
+            </div>
+          </header>
+        </div>
+
+        <div class name ="shadow one"></div>
+        <div class name ="shadow two"></div>
+      </div>
+
+      <div class name ="links">
+        <ul>
+          <li>
+            <a href="Home.html" style="--i: 0.05s;">Home</a> 
+          </li>
+          <li>
+            <a href="Services.html" style="--i: 0.1s;">Services</a>
+          </li>
+          <li>
+            <a href="Profile.html" style="--i: 0.15s;">Profile </a>
+          </li>
+          <li>
+            <a href="Trasmissions.html" style="--i: 0.2s;">Transmissions</a>
+          </li>
+          <li>
+            <a href="About.html" style="--i: 0.25s;">About</a>
+          </li>
+          <li>
+            <a href="Contact.html" style="--i: 0.3s;">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <script src="menu.js"></script>
+    
+        
+    </body>
+    export default  from index
